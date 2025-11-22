edfa705a857c3d326ab987d3fb92f5731df268cb
Xandxm <nls200700307@gmail.com>
2025-11-22 15:51:34 +0200

    LAB 6

---
+++ lab_6/lab_6_2/lab_6_2/lab_6_2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a, b;
+
+    std::cout << "Введите значение a: ";
+    std::cin >> a;
+    std::cout << "Введите значение b: ";
+    std::cin >> b;
+
+    int* ptr_a = &a;
+    int* ptr_b = &b;
+
+    *ptr_a *= 2;
+    std::cout << "Значение A, увеличенное через указатель в 2 раза: " << a << std::endl;
+
+    int temp = *ptr_a;
+    *ptr_a = *ptr_b;
+    *ptr_b = temp;
+
+    std::cout << "После операций: a = " << a << std::endl
+        << "b = " << b << std::endl;
+
+    return 0;
+}
